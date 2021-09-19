b6c74e93f4189e7a35e81b21f8a02b3b2bb71c88
jonasGN <j.goncalvesneto@outlook.com>
2021-09-19 11:36:40 -0300

    Audit data add

--- backend/src/main/java/jonas/gn/dscatalog/entities/Category.java
+++ backend/src/main/java/jonas/gn/dscatalog/entities/Category.java
@@ -1,13 +1,19 @@
 package jonas.gn.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
+import jonas.gn.dscatalog.entities.definitions.EntityDefinitions;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +25,12 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	@Column(columnDefinition = EntityDefinitions.withoutTimeZone)
+	private Instant createdAt;
+
+	@Column(columnDefinition = EntityDefinitions.withoutTimeZone)
+	private Instant updateAt;
 
 	public Category() {
 	}
@@ -42,6 +54,24 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updateAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();
 	}
 
 	@Override
---
+++ backend/src/main/java/jonas/gn/dscatalog/entities/definitions/EntityDefinitions.java
@@ -0,0 +1,5 @@
+package jonas.gn.dscatalog.entities.definitions;
+
+public final class EntityDefinitions {
+	public static final String withoutTimeZone = "TIMESTAMP WITHOUT TIME ZONE";
+}
