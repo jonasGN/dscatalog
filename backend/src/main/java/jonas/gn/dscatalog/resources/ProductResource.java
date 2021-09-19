f90484b6fe07672e918d4ce4b1e5adf6b142efc9
jonasGN <j.goncalvesneto@outlook.com>
2021-09-19 18:01:06 -0300

    Product POST, PUT and DELETE add

--- backend/src/main/java/jonas/gn/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/jonas/gn/dscatalog/resources/ProductResource.java
@@ -1,5 +1,7 @@
 package jonas.gn.dscatalog.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,9 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jonas.gn.dscatalog.dto.ProductDTO;
 import jonas.gn.dscatalog.services.ProductService;
@@ -23,11 +30,9 @@ public class ProductResource {
 	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> fetchAllPaged(
-			@RequestParam(defaultValue = "0") Integer page,
+	public ResponseEntity<Page<ProductDTO>> fetchAllPaged(@RequestParam(defaultValue = "0") Integer page,
 			@RequestParam(defaultValue = "20") Integer linesPerPage,
-			@RequestParam(defaultValue = "ASC") String direction, 
-			@RequestParam(defaultValue = "id") String orderBy) {
+			@RequestParam(defaultValue = "ASC") String direction, @RequestParam(defaultValue = "id") String orderBy) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
 		Page<ProductDTO> result = service.fetchAllPaged(pageRequest);
@@ -37,15 +42,32 @@ public class ProductResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> fetchById(@PathVariable Long id) {
-		ProductDTO entity = service.fetchById(id);
+		ProductDTO result = service.fetchById(id);
 
-		return ResponseEntity.ok(entity);
+		return ResponseEntity.ok(result);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> add(@RequestBody ProductDTO product) {
+		ProductDTO result = service.add(product);
+
+		UriComponentsBuilder uriBuilder = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}");
+		URI uri = uriBuilder.buildAndExpand(product.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(result);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO product) {
+		ProductDTO result = service.update(id, product);
+
+		return ResponseEntity.ok(result);
+	}
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
-		
+
 		return ResponseEntity.noContent().build();
 	}
 
--- backend/src/main/java/jonas/gn/dscatalog/services/ProductService.java
+++ backend/src/main/java/jonas/gn/dscatalog/services/ProductService.java
@@ -2,6 +2,8 @@ package jonas.gn.dscatalog.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -10,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jonas.gn.dscatalog.dto.CategoryDTO;
 import jonas.gn.dscatalog.dto.ProductDTO;
+import jonas.gn.dscatalog.entities.Category;
 import jonas.gn.dscatalog.entities.Product;
+import jonas.gn.dscatalog.repositories.CategoryRepository;
 import jonas.gn.dscatalog.repositories.ProductRepository;
 import jonas.gn.dscatalog.services.exceptions.DatabaseException;
 import jonas.gn.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -21,6 +26,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> fetchAllPaged(PageRequest pageRequest) {
@@ -37,6 +45,28 @@ public class ProductService {
 		return new ProductDTO(entity, entity.getCategories());
 	}
 
+	@Transactional
+	public ProductDTO add(ProductDTO product) {
+		Product entity = new Product();
+		copyDtoToEntity(product, entity);
+		entity = repository.save(entity);
+
+		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO product) {
+		try {
+			Product entity = repository.getById(id);
+			copyDtoToEntity(product, entity);
+			entity = repository.save(entity);
+
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException();
+		}
+	}
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
@@ -47,4 +77,17 @@ public class ProductService {
 		}
 	}
 
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setMoment(dto.getMoment());
+
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getById(catDto.getId());
+			entity.getCategories().add(category);
+		}
+	}
 }
